2e10625d086d8a6af569ec6364070c7cff3a3f1b
ortizj2 <joe.ortiz@adp.com>
2018-04-18 12:42:45 -0400

    verify-data-binding

--- force-app/main/default/staticresources/myTestSuite.js
+++ force-app/main/default/staticresources/myTestSuite.js
@@ -20,4 +20,19 @@ describe("Lightning Component Testing Examples", function() {
                 });
         });
     });
+
+    describe('c:componentWithDataBinding', function () {
+        it('verify data binding', function (done) {
+           $T.createComponent('c:componentWithDataBinding', {message: 'Hello World!'}, true)
+              .then(function (component) {
+                
+                 expect(component.find("message").getElement().innerHTML).toBe('Hello World!');
+                 expect(component.find("messageInput").get("v.value")).toBe('Hello World!');
+                 done();
+           }).catch(function (e) {
+                
+                 done.fail(e);
+           });
+        });
+     });
 });
